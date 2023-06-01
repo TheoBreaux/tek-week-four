1c4861796c393dc666d11957e6d049b85265dd9c
Theo Breaux <theobreaux@gmail.com>
2023-06-01 12:16:20 -0700

    pass by

---
+++ JavaScript_Pass_by_Lab/index.js
@@ -0,0 +1,25 @@
+const personOne = {
+  name: {
+    first: "Timmy",
+    last: "Timtim",
+  },
+  age: 30,
+  location: {
+    city: "New York",
+    state: "New York",
+    zip: 10001,
+  },
+};
+
+const personTwo = {
+  name: {
+    first: "Julie",
+    last: "July",
+  },
+  age: 28,
+  location: {
+    city: "Albany",
+    state: "New York",
+    zip: 12201,
+  },
+};
