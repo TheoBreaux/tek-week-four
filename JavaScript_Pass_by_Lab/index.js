a5217b200b31fde0d3548377a10945618a7306fb
Theo Breaux <theobreaux@gmail.com>
2023-06-02 08:06:11 -0700

    #2

--- JavaScript_Pass_by_Lab/index.js
+++ JavaScript_Pass_by_Lab/index.js
@@ -32,31 +32,32 @@ const moveLocation = (personObj, locationObj) => {
   personObj.location.zip = locationObj.zip;
 };
 
-// const nLoc = {
-//   city: "Rochester",
-//   state: "New York",
-//   zip: 14604,
-// };
+const newLoc = {
+  city: "Rochester",
+  state: "New York",
+  zip: 14604,
+};
 
-// moveLocation(personTwo, nLoc);
+moveLocation(personTwo, newLoc);
 
-// nLoc.city = "Buffalo";
-// nLoc.zip = 14201;
+newLoc.city = "Buffalo";
+newLoc.zip = 14201;
 
-// console.log(personTwo.location.city);
+console.log(personTwo.location.city);
 
 // 2 Julie and Timmy have gotten married! Since they'll be living together, we should give them the same location. However, since they'll be living together forever, let's save ourselves the hassle of updating both of their locations every time they move. Set Julie's location to Timmy's location by reference. Test this new link by using the following code, which utilizes our already-completed moveLocation() function with no changes necessary. Do both people now live in California? (They both should.)
 
 personTwo.location = personOne.location;
 
-const nLoc = {
+const newLoc2 = {
   city: "Mountain View",
   state: "California",
   zip: 94035,
 };
 
-moveLocation(personOne, nLoc);
+moveLocation(personOne, newLoc2);
+
 console.log(personOne.location);
 console.log(personTwo.location);
 
-// As you can see, sometimes linking objects by reference is useful. Now, whenever one person moves, the other follows.
+// // As you can see, sometimes linking objects by reference is useful. Now, whenever one person moves, the other follows.
